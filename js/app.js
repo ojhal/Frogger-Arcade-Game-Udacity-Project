744d4819cd65ae9aae4abb347af9622d5822e8e4
Ojhal Patwa <patkarojhal26@gmail.com>
2020-04-13 23:47:32 +0530

    add width and height

--- js/app.js
+++ js/app.js
@@ -1,51 +1,83 @@
+let debug = false;
+let game = true;
 // Enemies our player must avoid
-var Enemy = function(x,y,sprite) {
-	this.x = x;
-	this.y = y;
+var Enemy = function(x,y) {
+    this.x = x;
+    this.y = y;
+
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.height = 605;
+    this.width = 505;
 };
-
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    this.x += 150 * dt;
+
+
+
+    
 
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 };
 
-
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+var Player = function(x,y,sprite){
+    this.x = x;
+    this.y = y;
+    this.sprite = sprite;
+    this.height = 75;
+    this.width = 65;
+
+}
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x,y,sprite) {
-	this.x = x;
-	this.y = y;
-	this.sprite = 'images/char-boy.png';
-}
-Player.prototype.render = function() {
-	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
-Player.prototype.update = function(dt) {
 
-};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+Player.prototype.render = function() {
+}
+
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.handleInput = function(direction){
+    const horizontal = 101;
+        vertical = 83;
+
+
+    if (direction ==='left' && this.x + horizontal >= 0){
+        this.x -= horizontal;
+
+    }else if (direction === 'right' && this.x + horizontal < ctx.canvas.width){
+        this.x += horizontal;
+
+    }else if (direction === 'down' && this.y + vertical < ctx.canvas.height -200){
+        this.y += vertical;
+
+    }else if (direction === 'up'){
+        this.y -= vertical;
+    }
+}
 
 
 // This listens for key presses and sends the keys to your
@@ -60,5 +92,26 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-const player = [];
-const allEnemies = [];
+
+
+const enemyPosition = [55, 140, 230];
+
+
+const player = new Player(202, 400, 'images/char-boy.png');
+
+const allEnemies = enemyPosition.map((y, index) => {
+    return new Enemy( (+200 * (index +1)), y);
+});
+
+
+//for debugging
+
+function enemyBBox(enemy) {
+
+}
+
+
+function playerBBox(player) {
+
+}
+
